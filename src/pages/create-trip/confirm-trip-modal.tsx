90f7f268a6ba5ba55779ff6b6bebe1b7fc1c1105
Igor Fernandes <igorfernamdez@gmail.com>
2025-06-25 21:59:36 -0300

    exibir data correta de viagem no modal de confirmacao

--- src/pages/create-trip/confirm-trip-modal.tsx
+++ src/pages/create-trip/confirm-trip-modal.tsx
@@ -1,19 +1,26 @@
 import { User, X } from "lucide-react";
 import { FormEvent } from "react";
 import { Button } from "../../components/button";
+import { DateRange } from "react-day-picker";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 interface ConfirmTripModalProps {
   closeConfirmTripModal: () => void
   setOwnerName: (name: string) => void;
   setOwnerEmail: (email: string) => void;
   createTrip: (event: FormEvent<HTMLFormElement>) => void
+  destination: string;
+  eventStartAndEndDates: DateRange | undefined;
 }
 
 export function ConfirmTripModal({
   closeConfirmTripModal,
   createTrip,
   setOwnerEmail,
-  setOwnerName
+  setOwnerName,
+  destination,
+  eventStartAndEndDates
 }: ConfirmTripModalProps) {
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
@@ -27,7 +34,12 @@ export function ConfirmTripModal({
           </div>
 
           <p className="text-sm text-zinc-400">
-          Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">Florianópolis, Brasil</span> nas datas de <span className="font-semibold text-zinc-100">16 a 27 de Agosto de 2024</span> preencha seus dados abaixo:
+            Para concluir a criação da viagem para <span className="font-semibold text-zinc-100">{destination}</span> nas datas de <span className="font-semibold text-zinc-100">
+              {eventStartAndEndDates?.from && eventStartAndEndDates?.to 
+                ? `${format(eventStartAndEndDates.from, 'dd \'de\' MMMM \'de\' yyyy', { locale: ptBR })} até ${format(eventStartAndEndDates.to, 'dd \'de\' MMMM \'de\' yyyy', { locale: ptBR })}`
+                : 'datas não selecionadas'
+              }
+            </span> preencha seus dados abaixo:
           </p>
         </div>
         
